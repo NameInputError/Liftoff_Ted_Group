7f229005cb1e3436dcf6f10e36c6c0a9a37fb0f6
Susan <susandu924@gmail.com>
2022-12-23 20:39:46 -0600

    track-my-eats first commit

---
+++ src/main/java/org/launchcode/trackmyeats/TrackMyEatsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.trackmyeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMyEatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMyEatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/trackmyeats/TrackMyEatsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.trackmyeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMyEatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
