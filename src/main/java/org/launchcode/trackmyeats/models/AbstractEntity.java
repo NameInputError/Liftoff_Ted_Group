ea15fcfa28f43bf181d2b159df6080621734bb3f
Angie Zarinelli <angie.zarinelli@gmail.com>
2023-01-08 13:42:52 -0600

    no red copy

--- src/main/java/org/launchcode/trackmyeats/models/AbstractEntity.java
+++ src/main/java/org/launchcode/trackmyeats/models/AbstractEntity.java
@@ -1,33 +1,35 @@
-//package org.launchcode.trackmyeats.models;
-//
-//
-//
-//import org.launchcode.trackmyeats.models.User;
-//
-//import java.util.Objects;
-//import java.util.Optional;
-//
-//@MappedSuperclass
-//public abstract class AbstractEntity {
-//
-////    @Id
-//    @GeneratedValue
-//    private int id;
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        AbstractEntity that = (AbstractEntity) o;
-//        return id == that.id;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id);
-//    }
-//}
+package org.launchcode.trackmyeats.models;
+
+
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.MappedSuperclass;
+import org.launchcode.trackmyeats.models.User;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@MappedSuperclass
+public abstract class AbstractEntity {
+
+//    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AbstractEntity that = (AbstractEntity) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
