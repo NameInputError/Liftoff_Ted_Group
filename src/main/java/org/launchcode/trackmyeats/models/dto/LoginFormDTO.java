f160b2077aff8e095b62dc25ec5b0036b59061df
ElB5 <eb.brady.ellen@gmail.com>
2023-01-14 14:21:36 -0600

    -- changes made to login and register DTO's so that it no longer says "must not be blank", removed @NotNull from both DTO's. Removed page-header fragment from login and register form.

--- src/main/java/org/launchcode/trackmyeats/models/dto/LoginFormDTO.java
+++ src/main/java/org/launchcode/trackmyeats/models/dto/LoginFormDTO.java
@@ -6,13 +6,12 @@ import jakarta.validation.constraints.Size;
 
     public class LoginFormDTO {
 
-        @NotNull
-        @NotBlank
+        @NotBlank(message = "Username is required")
         @Size(min = 3, max = 20, message = "Invalid username. Must be between 3 and 20 characters.")
         private String username;
 
-        @NotNull
-        @NotBlank
+
+        @NotBlank(message = "Password is required")
         @Size(min = 5, max = 30, message = "Invalid password. Must be between 5 and 30 characters.")
         private String password;
 
