788b45fe5dc94e34dd79891e384a986ed8140a99
Angie Zarinelli <angie.zarinelli@gmail.com>
2023-01-27 20:53:01 -0600

    links added, colors added

--- src/main/java/org/launchcode/trackmyeats/models/Restaurants.java
+++ src/main/java/org/launchcode/trackmyeats/models/Restaurants.java
@@ -1,18 +1,18 @@
-//package org.launchcode.trackmyeats.models;
-//
-////public enum Categories {
-////    ITALIAN("Italian"), CHINESE("Chinese"), AMERICAN("American");
-//
-//public enum Restaurants{BIGGIES("Biggies"), WENDYS("Wendys"),
-//                        ELICIOUS("Elicious");
-//    private final String displayName;
-//
-//    Restaurants(String displayName) {
-//        this.displayName = displayName;
-//    }
-//
-//    public String getDisplayName() {
-//        return displayName;
-//    }
-//
-//}
+package org.launchcode.trackmyeats.models;
+
+//public enum Categories {
+//    ITALIAN("Italian"), CHINESE("Chinese"), AMERICAN("American");
+
+public enum Restaurants{BIGGIES("Biggies"), WENDYS("Wendys"),
+                        ELICIOUS("Elicious");
+    private final String displayName;
+
+    Restaurants(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+}
